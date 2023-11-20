8ee80c9421e2d113728198144a2e096c27315ea8
Thilac06 <132432326+Thilac06@users.noreply.github.com>
2023-11-20 11:53:24 +0530

    Create prov1.py

---
+++ prov1.py
@@ -0,0 +1,49 @@
+provinces = ["Northern", "North Central", "Eastern", "Uva", "Southern", "Western", "North Western", "Central", "Sabaragamuwa"]
+districts = [
+    ["Jaffna", "Kilinochchi", "Mullaitivu", "Mannar", "Vavuniya"],
+    ["Polonnaruwa", "Anuradhapura"],
+    ["Trincomalee", "Ampara", "Batticaloa"],
+    ["Badulla", "Moneragala"],
+    ["Galle", "Matara", "Hambantota"],
+    ["Colombo", "Gampaha", "Kalutara"],
+    ["Kurunegala", "Puttalam"],
+    ["Kandy", "Matale", "Nuwara-Eliya"],
+    ["Kegalle", "Ratnapura"]
+]
+
+type_of_las = ["PS", "UC", "MC"]
+
+local_authorities = [
+    "Chavakachcheri PS", "Chavakachcheri UC", "Delft PS", "Jaffna MC", "Karainagar PS", 
+    "Kayts PS", "Nallur PS", "Point Pedro PS", "Point Pedro UC", "Vadamaradchi South West PS", 
+    "Valikamam East PS", "Valikamam North PS", "Valikamam South PS", "Valikamam South West PS", 
+    "Valikamam West PS", "Valvetithurai UC", "Velanai PS", "Karachchi PS", "Pachchilaipalli PS", 
+    "Poonakary PS", "Manthai East PS", "Maritimepattu PS", "Puthukudiyiruppu PS", "Thunukkai PS", 
+    "Mannar PS", "Mannar UC", "Manthai West PS", "Musali PS", "Nanattan PS", "Vavuniya North PS", 
+    "Vavuniya South (Sinhala) PS", "Vavuniya South (Tamil) PS", "Vavuniya UC", "Vengalasettikula PS", 
+    "Anuradhapura MC", "Galenbindunuwewa PS", "Galnewa PS", "Horowpothana PS", "Ipalogama PS", 
+    "Kahatagasdigiliya PS", "Kebithigollewa PS", "Kekirawa PS", "Medawachchiya PS", "Mihintale PS", 
+    "Nochchiyagama PS", "Nuwaragam Palatha Central PS", "Nuwaragam Palatha East PS", "Padaviya PS", 
+    "Palagala PS", "Rajanganaya PS", "Rambewa PS", "Talawa PS", "Tirappane PS", "Dimbulagala PS", 
+    "Elehera PS", "Hingurakgoda PS", "Lankapura PS", "Medirigiriya PS", "Polonnaruwa MC", 
+    "Polonnaruwa PS", "Welikanda PS", "Gomarankadawala PS", "Kantale PS", "Kinniya PS", 
+    "Kinniya UC", "Kuchchaweli PS", "Morawewa PS", "Muttur PS", "Padavisiripura PS", "Seruvila PS", 
+    "Thambalagamuwa PS", "Trincomalee Town and Gravets PS", "Trincomalee UC", "Verugal PS", 
+    "Addalachenai PS", "Akkaraipattu MC", "Akkaraipattu PS", "Alaiyadivembu PS", "Ampara UC", 
+    "Damana PS", "Dehiattakandiya PS", "Irakkamam PS", "Kalmunai MC", "Karaitivu PS", "Lahugala PS", 
+    "Maha Oya PS", "Namal Oya PS", "Naveethanveli PS", "Ninthavur PS", "Padiyatalawa PS", "Pottuvil PS", 
+    "Sammanthurai PS", "Thirukkovil PS", "Uhana PS", "Batticaloa MC", "Eravur Pattu PS", "Eravur UC", 
+    "Kattankudi UC", "Koralaipattu North PS", "Koralaipattu PS", "Koralepattu West PS", 
+    "Manmunal Pattu PS", "Manmunal South and Eruvil Pattu PS", "Manmunal South West PS", 
+    "Manmunal West (Vavunatheev) PS", "Porativpattu PS", "Badulla MC", "Badulla PS", "Bandarawela MC", 
+    "Bandarawela PS", "Ealla PS", "Haldummulla PS", "Hali-Ela PS", "Haputale PS", "Haputale UC", 
+    "Kandeketiya PS", "Lunugala PS", "Mahiyanganaya PS", "Meegahakivula PS", "Passara PS", 
+    "Ridimaliyadda PS", "Soranathota PS", "Uva-Paranagama PS", "Welimada PS", "Badalkumbura PS", 
+    "Bibila PS", "Buttala PS", "Kataragama PS", "Madulla PS", "Medagama PS", "Moneragala PS", 
+    "Siyambalanduwa PS", "Tanamalwila PS", "Wellawaya PS", "Ambalangoda PS", "Karandeniya PS", 
+    "Rajgama PS", "Akmeemana PS", "Baddegama PS", "Niyagama PS", "Bentota PS", "Elpitiya PS", 
+    "Galle MC", "Habaraduwa PS", "Hikkaduwa PS", "Karapitiya PS", "Nagoda PS", "Ambagamuwa PS", 
+    "Balangoda PS", "Imbulpe PS", "Kolonne PS", "Kotakethana PS", "Ratnapura MC", "Weligepola PS",
+    "Ayagama PS", "Balangoda PS", "Embilipitiya UC", "Rathnapura MC"
+]
+
